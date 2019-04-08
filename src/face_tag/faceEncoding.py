142ba8ff108a51ff32e0eac80e34f575b4c794a9
장지은 <48377999+j0077@users.noreply.github.com>
2019-04-09 03:57:10 +0900

    revised face encoding processor

--- src/face_tag/faceEncoding.py
+++ src/face_tag/faceEncoding.py
@@ -1,6 +1,6 @@
 import face_recognition
 import sqlite3
-import faceClustering
+
 
 #input_new_image 파일에서 디렉토리 이름과 새로운 이미지 파일 리스트를 받아와서 이미지 경로 fullname 리스트 리턴
 def set_image_file_list(path_dir, new_image_file_list):
@@ -27,7 +27,7 @@ def get_face_vector(image, face_box):
     return face_vector_list
 
 #input_new_image파일에 호출되는 함수
-def face_recog(database_file, path_dir, new_image_file_list):
+def face_encoding(database_file, path_dir, new_image_file_list):
     #데이터베이스 열기
     con = sqlite3.connect(database_file)
     cur = con.cursor()
@@ -41,17 +41,19 @@ def face_recog(database_file, path_dir, new_image_file_list):
         #이미지에 detect된 얼굴이 없을 경우 pass
         if(len(face_boxes) == 0):
             pass
-        #이미지에 detect된 얼굴이 있으면 vector값을 받아서 db에 저
+        #이미지에 detect된 얼굴이 있으면 vector값을 받아서 db에 저장
         else:
-            face_vector_lists = get_face_vector(image, face_boxes)
-            list(face_boxes)
-            list(face_vector_lists)
+            face_vector_list = get_face_vector(image, face_boxes)
+            face_boxes
+            face_vector_list
             for n in range(0, len(face_boxes)):
                 top = int(face_boxes[n][0])
                 right = int(face_boxes[n][1])
                 bottom = int(face_boxes[n][2])
                 left = int(face_boxes[n][3])
                 sql = "INSERT INTO Face_Vector_Value(imageFileName,imageTop, imageRight, imageBottom, imageLeft, vectorValue) VALUES (?, ?, ?, ?, ?, ?)"
-                cur.execute(sql, (i,top, right, bottom, left, face_vector_lists[n]))
+                cur.execute(sql, (i,top, right, bottom, left, face_vector_list[n]))
                 con.commit()
+    #데이터베이스 닫기
+    cur.close()
     con.close()
