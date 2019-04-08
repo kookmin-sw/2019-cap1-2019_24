45dc8bebf2546a302f4ceb0f3149059f15aabdc5
MinnySon <48342798+MinnySon@users.noreply.github.com>
2019-04-08 22:23:01 +0900

    Update MainActivity.java

--- docs/src/main/java/com/example/WITTYPHOTOS/MainActivity.java
+++ docs/src/main/java/com/example/WITTYPHOTOS/MainActivity.java
@@ -6,25 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+
 import com.example.WITTYPHOTOS.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView test;
+    private ImageView album;
+    private ImageView graph;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        test = findViewById(R.id.test);
-        test.setOnClickListener(new View.OnClickListener() {
+
+        album = findViewById(R.id.album);
+        album.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,ImagePickerDirectoryActivity.class));
             }
         });
 
+        graph = findViewById(R.id.graph);
+        graph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,GraphDir.class));
+            }
+        });
+
     }
+
 
     @Override
     protected void onStop() {
