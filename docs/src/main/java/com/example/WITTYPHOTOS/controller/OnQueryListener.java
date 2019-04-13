3567a5edb8d3c4a6e71c1579e2e6b589174d9e4a
MinnySon <48342798+MinnySon@users.noreply.github.com>
2019-04-13 13:41:16 +0900

    Image Load Path Jave Code
    기기의 이미지들을 가져오는 코드입니다. 각 휴대폰에 생성되어있는 폴더를 그대로 가져오며 사진 개수 또한 카운팅 되어 보여집니다.

---
+++ docs/src/main/java/com/example/WITTYPHOTOS/controller/OnQueryListener.java
@@ -0,0 +1,10 @@
+package com.example.WITTYPHOTOS.controller;
+
+import com.example.WITTYPHOTOS.PickerDir;
+
+import java.util.ArrayList;
+
+public interface OnQueryListener {
+
+    void onComplete(ArrayList<PickerDir> data);
+}
