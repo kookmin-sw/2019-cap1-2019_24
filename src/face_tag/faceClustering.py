f5f0dbc5c277a2843f423b189781db7bf0227903
장지은 <48377999+j0077@users.noreply.github.com>
2019-04-15 06:34:21 +0900

    cluster, set_vectorID 등 함수 수정, t-sne 추가

--- src/face_tag/faceClustering.py
+++ src/face_tag/faceClustering.py
@@ -1,8 +1,12 @@
+
+# -*- coding: utf-8 -*-
 from sklearn.cluster import DBSCAN
 import numpy as np
 import sqlite3
 import array
-
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+import mglearn
 
 #데이터베이스에서 모든 vector 값을 가져온다.
 def set_vectorValue(database_file, con):
@@ -17,47 +21,58 @@ def set_vectorValue(database_file, con):
         doubles = array.array('d', row[0])
         face_vector_list.append(doubles)
     return face_vector_list
-#    return face_vector_list
+
+def set_vectorID(database_file, con):
+    vectorID_list = []
+    cur = con.cursor()
+    cur.execute("SELECT vectorID FROM Face_vector_value")
+    vids = cur.fetchall()
+    for vid in vids:
+        vectorID_list.append(vid[0])
 
 #vector값을 모두 클러스터링하고 데이터베이스에 label id 저장
 def cluster(face_vector_list, con):
     cur = con.cursor()
+
     #DBSCAN을 사용한 클러스터
     clt = DBSCAN(metric="euclidean", n_jobs=-1)
     clt.fit(face_vector_list)
-    labelIDs = np.unique(clt.labels_)
-    numUniqueFaces = len(np.where(labelIDs > -1)[0])
-    print("[INFO] # unique faces: {}".format(numUniqueFaces))
 
-    for labelID in labelIDs:
-        print("[INFO] faces for face ID: {}".format(labelID))
-        idxs = np.where(clt.labels_ == labelID)[0]
-    #분류한 값을 0,1, ... 차례대로 label을 매긴다 (비슷한 얼굴이 없으면 -1)
+    # 분류한 값을 0,1, ... 차례대로 label을 매긴다 (비슷한 얼굴이 없으면 -1)
     labelIDs = np.unique(clt.labels_)
     #분류된 label의 개수
     numUniqueFaces = len(np.where(labelIDs > -1)[0])
-    print("[INFO] # unique faces: {}".format(numUniqueFaces))
     for labelID in labelIDs:
-        print("[INFO] faces for face ID: {}".format(labelID))
-        idxs = np.where(clt.labels_ == labelID)[0]
-        #label id를 db에 저장
+        if(labelID != -1):
+            idxs = np.where(clt.labels_ == labelID)[0]
+            for idx in idxs:
+                cur.execute("UPDATE Face_vector_value SET label = " + str(labelID) + " WHERE vectorID = " + str(idx))
+                con.commit()
+
+'''
+    t-sne을 사용하여 그래프로 확인
+    model = TSNE(learning_rate=100)
+    transformed = model.fit_transform(face_vector_list)
+
+    xs = transformed[:, 0]
+    ys = transformed[:, 1]
+    plt.scatter(xs, ys )
+
+    plt.show()
+'''
 
 #데이터베이스에 label을 검색하여 label 중 기존에 저장된 이름(태그)이 있으면 같은 이름으로 저장
 def insert_tag_to_db (con):
     cur = con.cursor()
-    cur.execute("SELECT label from Face_Vector_Value")
-    label_list = cur.fetchall()
-    for l in label_list:
-        sql = "SELECT faceName from Face_Vector_value WHERE label = ?"
-        cur.execute(sql, l)
-        name = cur.fetone()
-        if (name is None) : pass
-        else:
-            for i in label_list:
-                if (l == i) :
-                    sql = "UPDATE Face_Vector_value SET faceName = ? WHERE lable = ?"
-                    cur.execute(sql, (name, i))
-                    con.commit()
+    cur.execute("SELECT faceName FROM Face_vector_value")
+    names = cur.fetchall()
+    for n in names:
+        if(n[0] is not None):
+            sql = "SELECT label FROM Face_Vector_value WHERE faceName = " + str(n[0])
+            cur.execute(sql)
+            label = cur.fetchone()
+            sql = "UPDATE face_vector_value SET faceName = n WHERE label =" + str(label)
+            cur.execute(sql)
 
 #시작 함수
 def face_clustering(database_file):
@@ -65,7 +80,8 @@ def face_clustering(database_file):
     con = sqlite3.connect(database_file)
     cur = con.cursor()
     #데이터베이스에 저장되어있는 모든 vector값 가져오기
-    face_vector_list = set_vectorValue(database_file, con)
+    face_vector_list = set_vectorValue(database_file,con)
+    vectorID_list = set_vectorID(database_file, con)
     #클러스터링후 label 저장
     cluster(face_vector_list, con)
     #데이터베이스에 label들을 비교하여 이름(태그)저장
@@ -73,6 +89,3 @@ def face_clustering(database_file):
     #데이터베이스 닫기
     cur.close()
     con.close()
-
-face_clustering('test.db')
-
