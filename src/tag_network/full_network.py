260ae143e01bf59484773d8f1826a53f3e9c3dec
Ariel-G-Lee <kv829ly@naver.com>
2019-04-15 23:05:47 +0900

    full_network 수정

--- src/tag_network/full_network.py
+++ src/tag_network/full_network.py
@@ -9,25 +9,23 @@ from networkx.algorithms.community import coverage, performance
 # 커뮤니티 리스트
 community_lst = []
 # 커뮤니티가 얼마나 잘 뽑혔는지 평가하는 지표
+# performance, coverage의 값을 확인한 다음
+# performance의 변화 폭이 작고, coverage가 충분히 클 때까지 자름
 coverage_lst, performance_lst = [], []
 
 # 방향성 없는 네트워크 형성
 G = nx.Graph()
 
-# G.add_nodes_from([2,3]) : 노드 2, 3 추가
-# G.add_nodes_from(fruits) : fruits list에 있는 항목들을 노드로 추가
 
-# 알파벳 A부터 0까지 노드로 추가
-G.add_nodes_from([chr(c) for c in range(ord('A'), ord('A')+18)])
+# 가중치를 포함한 노드 간의 링크 생성
+G.add_weighted_edges_from([('A', 'B', 3), ('A', 'C', 2), ('A', 'I', 3), ('B', 'C', 5), ('B', 'D', 1),
+                           ('B', 'E', 7), ('C', 'D', 2), ('B', 'G', 9), ('B', 'I', 1), ('C', 'K', 7),
+                           ('D', 'F', 9), ('C', 'I', 6), ('E', 'F', 4), ('E', 'G', 5),
+                           ('E', 'I', 1), ('H', 'I', 9), ('H', 'K', 1)])
 
-# 노드 간의 링크 생성
-G.add_edges_from([('A', n) for n in list(G.node())[:10]])
-G.add_edges_from([('G', n) for n in list(G.node())[11:]])
-G.add_edges_from([('B', 'J'), ('C', 'J'), ('C', 'H'), ('H', 'E'), ('E', 'D'), ('D', 'I'),
-                  ('I', 'F'), ('B', 'K'), ('K', 'F')])
-G.add_edges_from([('O', 'N'), ('N', 'L'), ('M', 'L'), ('O', 'K'), ('K', 'M')])
-G.add_edges_from([('A', 'K'), ('G', 'K'), ('R', 'S'), ('E', 'S'), ('S', 'A')])
-G.add_edges_from([('G', 'C'), ('G', 'K'), ('B', 'C'), ('B', 'E'), ('P', 'Q'), ('P', 'N')])
+# 노드 삭제 코드
+# G.remove_node('K')
+# G.remove_nodes_from(['A', 'G'])
 
 # 모든 node와 edge 확인
 # print(G.nodes())
@@ -36,8 +34,6 @@ G.add_edges_from([('G', 'C'), ('G', 'K'), ('B', 'C'), ('B', 'E'), ('P', 'Q'), ('
 # G를 그래프로 표현
 pos = nx.spring_layout(G)
 
-# G.remove_node('K')
-# G.remove_nodes_from(['A', 'G'])
 
 for i, comms in enumerate(girvan_newman(G)):
     # performance의 변화폭이 많이 적어지면 더이상 cluster를 나누어도 이득이 없으므로 멈춤
@@ -47,21 +43,20 @@ for i, comms in enumerate(girvan_newman(G)):
         community_lst.append(comms),
         coverage_lst.append(coverage(G, comms)), performance_lst.append(performance(G, comms))
 
-# performance, coverage의 값을 확인한 다음
-# performance의 변화 폭이 작고, coverage가 충분히 클 때까지 자름
-'''
-분류된 커뮤니티 확인
+
+# 나눠진 커뮤니티 그룹 확인
 for s in community_lst[-1]:
     print(s)
-print(len(community_lst[-1]))
-'''
+# print(len(community_lst[-1]))
+
+selected_community = community_lst[-1]
 
 # 원래 그래프의 노드의 attribute에 community 정보를 넘겨줌
 for i, comm in enumerate(community_lst[-1]):
     for p in comm:
         G.nodes()[p]['community'] = i
 
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(8, 4))
 pos = nx.spring_layout(G)
 
 nx.draw_networkx_nodes(G, pos=pos, node_size=200,
@@ -70,9 +65,25 @@ nx.draw_networkx_nodes(G, pos=pos, node_size=200,
                        alpha=0.5, labels={n[0]: n[1]['community'] for n in G.nodes(data=True)})
 
 nx.draw_networkx_edges(G, pos=pos, edge_color='gray', edge_cmap=plt.cm.binary)
-tempx, tempy = 1.3, 0.0
-for c in plt.cm.gist_rainbow(np.linspace(0.0, 1.0, len(community_lst[-1]))):
-    plt.scatter(tempx, tempy, s=100, marker='o', alpha=0.5, zorder=0)
-plt.scatter(tempx, tempy, s=100, marker='o', c='white',  zorder=1)
+
+pos_min = (min((x for x, y in pos.values())), min((y for x, y in pos.values())))
+pos_max = (max((x for x, y in pos.values())), max((y for x, y in pos.values())))
+
+plt.xlim(pos_min[0]-0.1, pos_max[0]+0.1)
+plt.ylim(pos_min[1]-0.1, pos_max[1]+0.1)
+
+# 옆에 표시할 라벨 목록(분류된 그룹 수만큼 생성되도록 해야함)
+community_label_lst = ['set1', 'set2', 'set3', 'set4']
+
+tempx, tempy = 100, 100
+for i, c in enumerate(plt.cm.gist_rainbow(np.linspace(0.0, 1.0, len(selected_community)))):
+    # s 도형의 크기, marker 도형 모양, alpha 불투명도(0투명, 1불투명)
+    plt.scatter(tempx, tempy, s=100, marker='o', label=community_label_lst[i], alpha=0.5, zorder=0)
+plt.scatter(tempx, tempy, s=300, marker='o', c='white',  zorder=1)
 plt.legend()
 plt.show()
+
+'''
+안드로이드에서 그래프를 그려줄 예정이므로
+분류된 노드가 묶인 그룹ID를 같이 데이터로 넘겨줘야할 듯
+'''
