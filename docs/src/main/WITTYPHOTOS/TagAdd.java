be4f143cb64f36be2c7e2f981c13f67b82b7e320
MinnySon <48342798+MinnySon@users.noreply.github.com>
2019-05-17 11:14:36 +0900

    Update TagAdd.java

--- docs/src/main/WITTYPHOTOS/TagAdd.java
+++ docs/src/main/WITTYPHOTOS/TagAdd.java
@@ -1,15 +1,33 @@
 package com.example.WITTYPHOTOS;
 
 import android.content.DialogInterface;
+import android.net.Uri;
 import android.nfc.Tag;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.Button;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.SparseArray;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
 
 
 public class TagAdd extends AppCompatActivity implements View.OnClickListener {
@@ -27,12 +45,65 @@ public class TagAdd extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.activity_tag_add);
         btnInput = (Button)findViewById(R.id.btn_input);
         imageView = findViewById(R.id.imageView);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inMutable=true;
+        Bitmap mBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.id.imageView,
+                options);
+
+        Paint mRectPaint = new Paint();
+        mRectPaint.setStrokeWidth(5);
+        mRectPaint.setColor(Color.BLACK);
+        mRectPaint.setStyle(Paint.Style.STROKE);
+
+        Bitmap tempBitmap = Bitmap.createBitmap(mBitmap.getWidth(), mBitmap.getHeight(),
+                Bitmap.Config.RGB_565);
+
+        Canvas tempCanvas = new Canvas(tempBitmap);
+        tempCanvas.drawBitmap(mBitmap, 0,0, null);
+
+        FaceDetector faceDetector = new FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
+                .build();
+        if(!faceDetector.isOperational()) {
+            new AlertDialog.Builder(imageView.getContext()).setMessage("Could not set up the face" +
+                    "detector!").show();
+            return ;
+        }
+
+        Frame frame = new Frame.Builder().setBitmap(mBitmap).build();
+        SparseArray<Face> faces = faceDetector.detect(frame);
+
+        for(int i = 0; i < faces.size(); i++) {
+            Face thisface = faces.valueAt(i);
+
+            float x1 = thisface.getPosition().x;
+            float y1 = thisface.getPosition().y;
+            float x2 = x1 + thisface.getWidth();
+            float y2 = y1 + thisface.getHeight();
+            tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, mRectPaint) ;
+        }
+
+        imageView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
+
+
+
+
         btnInput.setOnClickListener(this);
-        imgPath = getIntent().getParcelableExtra("path);
-                                
-        Glide.with(this).load(imgPath).apply(new RequestOptions().centerInside().diskCacheStrategy(DiskCacheStrategy.ALL)).into(imageView);
-                                                 
-                                               
+        imgPath = getIntent().getParcelableExtra("path");
+
+        Glide.with(this)
+                .load(imgPath)
+                .apply(new RequestOptions()
+                        .centerInside()
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)).into(imageView);
+
+
+
+
+
+
     }
 
     @Override
@@ -58,6 +129,7 @@ public class TagAdd extends AppCompatActivity implements View.OnClickListener {
                     dialog.cancel();
                 }
             });
+            // on this part, we need to delete specific tag data, not cancel actions.
 
             dialog.show();
         }
