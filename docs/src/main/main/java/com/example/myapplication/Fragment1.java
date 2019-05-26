3f9221d06852934b960140755129e0a088a6f619
jung-chanyoung <wjdckfrl55@naver.com>
2019-05-26 17:24:18 +0900

    Add files via upload

---
+++ docs/src/main/main/java/com/example/myapplication/Fragment1.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+    public Fragment1() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
+        return rootView;
+        //xml 레이아웃이 인플레이트 되고 자바소스 코드와 연결이된다.
+    }
+}
