507da36b9acc6de75c5812675bde22e3c63905d1
장지은 <48377999+j0077@users.noreply.github.com>
2019-05-28 10:55:05 +0900

    Update views.py

--- wittyPhotosServer/wittyproject/objectDetect/views.py
+++ wittyPhotosServer/wittyproject/objectDetect/views.py
@@ -1,6 +1,9 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 import cv2 as cv
 import numpy as np
+import os
+
 
 # Initialize the parameters
 confThreshold = 0.5  # Confidence threshold
@@ -9,24 +12,31 @@ inpWidth = 416  # Width of network's input image
 inpHeight = 416  # Height of network's input image
 
 # Load names of classes
-classesFile = "./objectDetect/coco.names"
+classesFile = "/home/jangjieun/wittyphotos/wittyServer/objectDetect/coco.names"
 classes = None
 with open(classesFile, 'rt') as f:
     classes = f.read().rstrip('\n').split('\n')
 
 # Give the configuration and weight files for the model and load the network using them.
-modelConfiguration = "./objectDetect/yolov3.cfg"
-modelWeights = "./objectDetect/yolov3.weights"
+modelConfiguration = "/home/jangjieun/wittyphotos/wittyServer/objectDetect/yolov3.cfg"
+modelWeights = "/home/jangjieun/wittyphotos/wittyServer/objectDetect/yolov3.weights"
 
 net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
 net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 
 
+@csrf_exempt
 def returnAutoTag(request):
-    imageFileName = request.GET.get('imageFileName')
-    tagData = object_detect(imageFileName)
-    return JsonResponse(tagData)
+    path_dir = "/home/jangjieun/wittyphotos/wittyServer/media/media/"
+    file_list = os.listdir(path_dir)
+    tagInfoList = {
+        "tagDatas" : []
+        }
+    for i in file_list:
+        tagData = object_detect(i, path_dir)
+        tagInfoList["tagDatas"].append(tagData)
+    return JsonResponse(tagInfoList)
 
 # Get the names of the output layers
 def getOutputsNames(net):
@@ -34,7 +44,6 @@ def getOutputsNames(net):
     layersNames = net.getLayerNames()
     # Get the names of the output layers, i.e. the layers with unconnected outputs
     return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
 
 # Remove the bounding boxes with low confidence using non-maxima suppression
 def postprocess(frame, outs):
@@ -71,19 +80,19 @@ def postprocess(frame, outs):
         top = box[1]
         width = box[2]
         height = box[3]
-
     return classIds
 
-def object_detect(imageFileName):
-    img = cv.imread(imageFileName,1)
-    blob = cv.dnn.blobFromImage(img, 1 / 255, (inpWidth, inpHeight), [0, 0, 0], 1, crop=False)
+def object_detect(imageFileName, path_dir):
+    f_imageFileName = path_dir + imageFileName
+    img = cv.imread(f_imageFileName,1)
+    blob = cv.dnn.blobFromImage(img, 1 / 255, (inpWidth, inpHeight), [0, 0, 0], 1, crop=True)
     net.setInput(blob)
     outs = net.forward(getOutputsNames(net))
     classIds = postprocess(img, outs)
     tagData = {
-        'imageFileName': imageFileName,
-        'tag' : []
-    }
+        'imageFileName' : imageFileName,
+        'tagList' : [],
+        }
     for classId in classIds:
-        tagData['tag'].append(classes[classId])
+        tagData['tagList'].append(classes[classId])
     return tagData
