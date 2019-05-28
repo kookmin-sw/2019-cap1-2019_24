b7105a08d3cc54040075b8e4f8375557d3e73974
장지은 <48377999+j0077@users.noreply.github.com>
2019-05-28 10:54:00 +0900

    Update views.py

--- wittyPhotosServer/wittyproject/faceRecog/views.py
+++ wittyPhotosServer/wittyproject/faceRecog/views.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
 import face_recognition
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import cv2 as cv
-import numpy as np
 import os
+
+faceInfoList = {
+    "faceInfos" : []
+    }
 
 @csrf_exempt
 def returnFaceInfo(request):
     path_dir = "/home/jangjieun/wittyphotos/wittyServer/media/media/"
     file_list = os.listdir(path_dir)
-    imageFileName = ""
-    tagDatas = {
+
+    global faceInfoList
+    faceInfoList = {
+        "faceInfos" : []
         }
     for i in file_list:
-        imageFileName = path_dir + i
+        face_recog(i, path_dir)
+        os.remove(path_dir + i)
+
+    return JsonResponse(faceInfoList)
 
 #이미지 파일 이름을 입력받아 face_recongnition의 load_image_file을 사용하여 입력받은 이미지 파일의 위치를 찾아가서 이미지 return
 def get_image(imageFileName):
@@ -34,33 +40,33 @@ def get_face_vector(image, face_box):
     face_vector_list = face_recognition.face_encodings(image, face_box)
     return face_vector_list
 
-def face_recog(imageFileName):
-        # faceInfo = {
-        #     'top': imageFileName,
-        #     'right' : [],
-        #     'bottom' : ,
-        #     'left' : ,
-        #     ''v
-        # }
-        #각 이미지마다 face detect 및 box 생성
-        image = get_image(imageFileName)
-        face_boxes = get_face_box(image)
-        #이미지에 detect된 얼굴이 없을 경우 pass
-        if(len(face_boxes) == 0):
-            pass
-        #이미지에 detect된 얼굴이 있으면 vector값을 받아서 db에 저장
-        else:
-            face_vector_list = get_face_vector(image, face_boxes)
-            for n in range(0, len(face_boxes)):
-                top = int(face_boxes[n][0])
-                right = int(face_boxes[n][1])
-                bottom = int(face_boxes[n][2])
-                left = int(face_boxes[n][3])
+def face_recog(imageFileName, path_dir):
+    f_imageFileName = path_dir + imageFileName
 
-                faceInfo = {
-                    'top': int(face_boxes[n][0]),
-                    'right' : int(face_boxes[n][1]),
-                    'bottom' : int(face_boxes[n][2]),
-                    'left' : int(face_boxes[n][3]),
-                    'vector' : face_vector_list[n],
-                }
+    global faceInfoList
+
+    #각 이미지마다 face detect 및 box 생성
+    image = get_image(f_imageFileName)
+    face_boxes = get_face_box(image)
+
+    #이미지에 detect된 얼굴이 없을 경우 pass
+    if(len(face_boxes) == 0):
+        pass
+
+    #이미지에 detect된 얼굴이 있으면 vector값을 받아서 db에 저장
+    else:
+        face_vector_list = get_face_vector(image, face_boxes)
+        for n in range(0, len(face_boxes)):
+            top = int(face_boxes[n][0])
+            right = int(face_boxes[n][1])
+            bottom = int(face_boxes[n][2])
+            left = int(face_boxes[n][3])
+            faceInfo = {
+                'imageFileName' : imageFileName,
+                'top': top,
+                'right' : right,
+                'bottom' : bottom,
+                'left' : left,
+                'vector' : str(face_vector_list[n]),
+            }
+            faceInfoList["faceInfos"].append(faceInfo)
