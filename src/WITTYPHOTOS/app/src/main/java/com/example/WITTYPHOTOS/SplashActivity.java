d4b2be4d00ec360d836bd8d2e77715570dcb0679
jung-chanyoung <wjdckfrl55@naver.com>
2019-05-30 21:04:05 +0900

    Update SplashActivity.java

--- src/WITTYPHOTOS/app/src/main/java/com/example/WITTYPHOTOS/SplashActivity.java
+++ src/WITTYPHOTOS/app/src/main/java/com/example/WITTYPHOTOS/SplashActivity.java
@@ -15,6 +15,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         startService(new Intent(SplashActivity.this,ServerConnection.class));
+        tagManager tagM;
 
         new Handler().postDelayed(new Runnable() {
             @Override
